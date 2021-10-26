edbee0ce558a51061d2f540bda054611b7210007
HarashimaYoshikazu <harashima318@gmail.com>
2021-10-26 17:16:32 +0900

    プレイヤーの視点を3人称に、マウスで視点移動可能に

---
+++ 3D Action/Assets/Scripts/Player/MouseCamera.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class MouseCamera : MonoBehaviour
+{
+    [SerializeField] float _moveSpeed = 3;
+    [SerializeField] float _jumpSpeed = 3;
+    Rigidbody _rb = default;
+    bool _isGrounded = true;
+    Animator _anim = default;
+
+    [SerializeField] Transform player;
+    [SerializeField] Transform eye;
+    [SerializeField] AxisState vertical;
+    [SerializeField] AxisState horizontal;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+        _anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 dir = Vector3.forward * v + Vector3.right * h;
+        // カメラのローカル座標系を基準に dir を変換する
+        dir = Camera.main.transform.TransformDirection(dir);
+        // カメラは斜め下に向いているので、Y 軸の値を 0 にして「XZ 平面上のベクトル」にする
+        dir.y = 0;
+        // 移動の入力がない時は回転させない。入力がある時はその方向にキャラクターを向ける。
+        if (dir != Vector3.zero) this.transform.forward = dir;
+        // 水平方向（XZ平面上）の速度を計算する
+        dir = dir.normalized * _moveSpeed;
+        // 垂直方向の速度を計算する
+        float y = _rb.velocity.y;
+
+
+        _rb.velocity = dir * _moveSpeed + Vector3.up * y;
+
+
+        horizontal.Update(Time.deltaTime);
+        vertical.Update(Time.deltaTime);
+
+        var horaizontalRotation = Quaternion.AngleAxis(horizontal.Value,Vector3.up);
+        var vaerticalRotation = Quaternion.AngleAxis(vertical.Value,Vector3.right);
+
+        player.rotation = horaizontalRotation;
+        eye.localRotation = vaerticalRotation;
+
+        
+        if (Input.GetButtonDown("Jump") && _isGrounded)
+        {
+            y = _jumpSpeed;
+        }
+        
+
+        if (_anim)
+        {
+            Vector3 walkSpeed = _rb.velocity;
+            walkSpeed.y = 0;
+            _anim.SetFloat("Speed", walkSpeed.magnitude);
+        }
+        _anim.SetFloat("X", h);
+        _anim.SetFloat("Y", v);
+    }
+}
