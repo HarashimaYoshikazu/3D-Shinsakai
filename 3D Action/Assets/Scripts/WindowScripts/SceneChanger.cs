ee158fcdd583f52da2eae95e713df1a57c13342b
HarashimaYoshikazu <harashima318@gmail.com>
2021-10-27 19:35:10 +0900

    ついか

--- 3D Action/Assets/Scripts/WindowScripts/SceneChanger.cs
+++ 3D Action/Assets/Scripts/WindowScripts/SceneChanger.cs
@@ -2,10 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using DG.Tweening;
 
 public class SceneChanger : MonoBehaviour
 {
-    public void Change(string scenename)
+    [SerializeField] string scenename;
+    [SerializeField] GameObject panel;
+    public void Change()
+    {
+        var fadeImage = panel.gameObject.GetComponent<Image>();
+        fadeImage.enabled = true;
+        var c = fadeImage.color;
+        c.a = 1.0f; // 初期値
+        fadeImage.color = c;
+
+        DOTween.ToAlpha(
+            () => fadeImage.color,
+            color => fadeImage.color = color,
+            0f, // 目標値
+            1f // 所要時間
+        ).OnComplete(SceneChange);
+        
+    }
+    private void SceneChange()
     {
         SceneManager.LoadScene(scenename);
     }
