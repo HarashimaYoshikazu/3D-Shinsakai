31d3e653b35f4dd9ac199974da933beb140a63d6
HarashimaYoshikazu <harashima318@gmail.com>
2021-11-08 17:18:36 +0900

    ダッシュの追加、戦闘モーションアセットの追加

--- 3D Action/Assets/Scripts/Player/MouseCamera.cs
+++ 3D Action/Assets/Scripts/Player/MouseCamera.cs
@@ -5,7 +5,9 @@ using Cinemachine;
 
 public class MouseCamera : MonoBehaviour
 {
-    [SerializeField] float _moveSpeed = 3;
+    [SerializeField] float _moveSpeed = 1;
+    [SerializeField] float _dushSpeed = 3;
+    float _currentSpeed = default;
     [SerializeField] float _jumpSpeed = 3;
     [SerializeField] float _damptime = 0.1f;
     Rigidbody _rb = default;
@@ -40,6 +42,18 @@ public class MouseCamera : MonoBehaviour
         dir = dir.normalized * _moveSpeed;
         // 垂直方向の速度を計算する
         float y = _rb.velocity.y;
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            _currentSpeed = _moveSpeed;
+            _moveSpeed = _dushSpeed;
+            _anim.SetBool("isRun", true);
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            _moveSpeed = _currentSpeed;
+            _anim.SetBool("isRun", false);
+        }
 
 
         _rb.velocity = dir * _moveSpeed + Vector3.up * y;
