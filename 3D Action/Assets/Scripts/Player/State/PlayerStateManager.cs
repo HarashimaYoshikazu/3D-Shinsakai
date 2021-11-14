2d3d7043cf6f6cdf9ea8859aa3612a0840859261
Harashima <harashima318@gmail.com>
2021-11-14 20:15:56 +0900

    tuika

---
+++ 3D Action/Assets/Scripts/Player/State/PlayerStateManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStateManager : MonoBehaviour
+{
+    static int hp;
+    static int attack;
+    static int gold;
+    public static int Hp { get => hp; set => hp = value; }
+    public static int Attack { get => attack; set => attack = value; }
+    public static int Gold { get => gold; set => gold = value; }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
