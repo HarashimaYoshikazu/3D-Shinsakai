8ec82313db087359ac6b4007fa92be4a9492f0f0
HarashimaYoshikazu <harashima318@gmail.com>
2022-02-17 20:16:01 +0900

    [add]SoundManagerの追加

---
+++ 3D Action/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : DDOLSingleton<SoundManager>
+{
+    [SerializeField,Tooltip("このオブジェクトのAudioSource")]
+    AudioSource _audioSource;
+
+
+    [SerializeField, Tooltip("選ぶ時のSE")]
+    AudioClip _selectSound;
+    public AudioClip SelectSound => _selectSound;
+
+    [SerializeField,Tooltip("タイトルのBGM")]
+    AudioClip _titleBGM;
+    public AudioClip TitleBGM => _titleBGM;
+
+    public void SoundPlay(AudioClip clip)
+    {
+        _audioSource.PlayOneShot(clip);
+    }
+}
